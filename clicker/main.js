f760ad4723c1197f2f94f6ae2d2e8a2bcd463bda
Popugai-grisha <108370874+Popugai-grisha@users.noreply.github.com>
2025-02-17 12:08:42 +0200

    Добавил немного

--- clicker/main.js
+++ clicker/main.js
@@ -1,4 +1,11 @@
 let noneImg = '/img/01 Images/Placeholder Square.svg';
-let imgOnStatsBlock = document.querySelector('.stats-box__img');
+let imgOnStatsBlock = document.querySelector('.stats-img');
+let clickerCounter = 0
 
 imgOnStatsBlock.setAttribute("src",noneImg);
+
+function clickerButton () {
+    console.log('Була натыскнута кнопка')
+    clickerCounter++
+    console.log(clickerCounter)
+}
